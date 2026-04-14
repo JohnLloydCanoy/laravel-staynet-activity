26655c0b18823b1b1ea7df0fd11a10c8178c98c7
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-14 22:25:30 +0800

    refactor Customer model to enable mass assignment and include HasFactory trait

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use HasFactory; // THIS IS THE MAGIC LINE
+
+    // Allowing mass assignment for your create/update forms
+    protected $fillable = [
+        'name',
+        'address',
+        'gender',
+        'dob', // or 'date_of_birth' depending on your exact setup
+    ];
 }
