7ba80e125d04c08ad6632dcb28f662b089e03926
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-16 17:54:23 +0800

    refactor LoanTransactionController to implement search functionality and improve code formatting

--- app/Http/Controllers/LoanTransactionController.php
+++ app/Http/Controllers/LoanTransactionController.php
@@ -5,26 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Customer;
 use App\Models\Loan;
+use App\Models\LoanTransaction;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class LoanTransactionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->search;
+
+        $transactions = LoanTransaction::with(['customer', 'loan'])
+            ->when($search, function ($query) use ($search) {
+                $query->whereHas('customer', function ($q) use ($search) {
+                    $q->where('name', 'like', "%{$search}%");
+                });
+            })->get();
+
+        return view('loantransactions.index', compact('transactions'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
-{
-    $customers = Customer::all();
-    $loans = Loan::all();
-    return view('loantransactions.create', compact('customers', 'loans'));
-}
+    {
+        $customers = Customer::all();
+        $loans = Loan::all();
+        return view('loantransactions.create', compact('customers', 'loans'));
+    }
     /**
      * Store a newly created resource in storage.
      */
@@ -64,4 +75,12 @@ class LoanTransactionController extends Controller
     {
         //
     }
+    public function generatePDF()
+{
+    $transactions = LoanTransaction::with(['customer', 'loan'])->get();
+    $pdf = Pdf::loadView('loantransactions.pdf', compact('transactions'));
+    
+    return $pdf->download('transactions_report.pdf');
+}
+
 }
