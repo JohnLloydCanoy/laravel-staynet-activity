47d7a4c05f2e1c7f229486589a99d40480d61bfc
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-16 17:54:29 +0800

    add loan and transaction routes with PDF generation functionality

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+// Existing Controllers
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
+
+// NEW: Activity 13 Controllers
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\LoanTransactionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,16 +21,21 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile Routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
-    // 1. ADD THE PDF ROUTE HERE (Before the resource route)
+    // Customer Routes (Activity 12)
     Route::get('/customers/export-pdf', [CustomerController::class, 'exportPDF'])->name('customers.pdf');
-    
-    // We replaced the single GET route with the full Resource route here:
     Route::resource('customers', CustomerController::class);
     
+    // NEW: Loan & Transaction Routes (Activity 13)
+    Route::get('/loantransactions/pdf', [LoanTransactionController::class, 'generatePDF'])->name('loantransactions.pdf');
+    Route::resource('loans', LoanController::class);
+    Route::resource('loantransactions', LoanTransactionController::class);
+    
+    // Other Existing Routes
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
 });
