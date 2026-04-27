dd8e24694e29bde938f6d6c391c41091758e95ba
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-27 19:44:44 +0800

    refactor LoanController to improve method documentation and clean up code

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -2,64 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Loan;
 use Illuminate\Http\Request;
-
 
 class LoanController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the loans.
      */
     public function index()
     {
-        //
+        $loans = Loan::latest()->get();
+        return view('loans.index', compact('loans'));
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new loan.
      */
     public function create()
     {
-        //
+        return view('loans.create');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created loan in storage.
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'description' => 'required|string|max:255',
+            'amount' => 'required|numeric',
+            'term' => 'required|integer',
+            'interest' => 'required|numeric',
+            'dategranted' => 'required|date',
+        ]);
+
+        Loan::create($request->all());
+
+        return redirect()->route('loans.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    // You can leave the edit, update, show, and destroy methods blank 
+    // for now unless you plan to add edit/delete buttons later!
+    public function show(Loan $loan) {}
+    public function edit(Loan $loan) {}
+    public function update(Request $request, Loan $loan) {}
+    public function destroy(Loan $loan) {}
 }
